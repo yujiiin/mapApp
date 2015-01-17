ea081a90b0865f79a519e026457f6b74b96e880d
Yuji Shimizu <yuji.shimizu@dena.com>
2015-01-16 22:21:37 -0800

    Initial Commit

---
+++ Map/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Map
+//
+//  Created by Yuji Shimizu on 1/16/15.
+//  Copyright (c) 2015 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map
+//
+//  Created by Yuji Shimizu on 1/16/15.
+//  Copyright (c) 2015 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
