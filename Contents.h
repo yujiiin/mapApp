b7dfacdf73794263147efcbfa78c5b6ab9f0d75a
Yuji Shimizu <yuji06082007@gmail.com>
2015-02-01 22:40:15 -0800

    Delete Contents.h

--- Contents.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface Contents : NSObject
-+ (NSArray *)loadContents;
-+ (NSDictionary *)newContent:(Class) class withTitle:(NSString *)title andDescription:(NSString *)description;
-@end
